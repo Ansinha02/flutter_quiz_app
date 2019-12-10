302eeee6fcc76acdadd0c0e9d99c2953179b4b78
ansinha02 <abhinavkr621@outlook.com>
2019-12-10 22:52:07 +0530

    first stateful widget, needs improvement.

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion(){
+
+    setState(() {
+       questionIndex = questionIndex + 1;
+    });
+   
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your favourite color?",
+      "What's your favourite animal?",
+      "What is your favourite Chocolate?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Ultimate Quizzer!"),
         ),
-        body: Text("This is just a default text"),
+        body: Column(
+          children: [
+            Text(questions.elementAt(questionIndex)),
+            RaisedButton(
+              child: Text("Ans 1."),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Ans 2."),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Ans 3."),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
